9b4ad499d6427ecf4858a82498a0a2bd6acb673a
tolking <qw13131wang@gmail.com>
2023-07-26 22:30:48 +0800

    chore: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+    ecmaVersion: 10,
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    'no-console': 'error',
+    'no-debugger': 'error',
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'vue/multi-word-component-names': 'off',
+  },
+}
--- examples/[page].paths.ts
+++ examples/[page].paths.ts
@@ -6,14 +6,14 @@ export default {
       .readdirSync(__dirname)
       .filter((file) => file.startsWith('['))
       .map((pkg, index) => {
-        console.log('pkg', pkg);
-        
+        // console.log('pkg', pkg);
+
         // TODO: 根据当前也数据的条数生成page
         if (index) {
-          return { params: { page: 'page/' + index, index, limit: 12 }}
+          return { params: { page: 'page/' + index, index, limit: 12 } }
         } else {
-          return { params: { page: 'index', index, limit: 12 }}
+          return { params: { page: 'index', index, limit: 12 } }
         }
       })
-  }
+  },
 }
