00938d5ebbd0eda3a2c1e1e98e8f7b0d4fcf7169
qiang <qw13131wang@gmail.com>
2023-08-12 23:49:30 +0800

    feat: optimize styles (#7)

--- src/components/VPCover.vue
+++ src/components/VPCover.vue
@@ -106,21 +106,21 @@ const coverStyle = computed(() => {
   max-width: 80%;
   font-size: clamp(2rem, 1.636rem + 1.82vw, 3rem);
   color: var(--vp-c-text-1);
+  mix-blend-mode: difference;
   line-height: 2;
   font-weight: 800;
   white-space: nowrap;
   text-overflow: ellipsis;
   overflow: hidden;
-  filter: drop-shadow(0px 4px 6px var(--vp-c-text-1));
 }
 .cover .cover-title .description {
   max-width: 80%;
   font-size: 1.2rem;
   color: var(--vp-c-text-2);
+  mix-blend-mode: difference;
   white-space: nowrap;
   text-overflow: ellipsis;
   overflow: hidden;
-  filter: drop-shadow(0px 2px 3px var(--vp-c-text-2));
 }
 
 .cover.with-cover {
