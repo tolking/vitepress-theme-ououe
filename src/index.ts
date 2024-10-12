b18ec1307c65cd25fafe798c05272ce2825a70f9
qiang <qw13131wang@gmail.com>
2024-10-12 16:24:36 +0800

    fix: miss the icons

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import type { Theme } from 'vitepress'
 
 import 'vitepress/dist/client/theme-default/styles/vars.css'
 import 'vitepress/dist/client/theme-default/styles/base.css'
+import 'vitepress/dist/client/theme-default/styles/fonts.css'
+import 'vitepress/dist/client/theme-default/styles/icons.css'
 import 'vitepress/dist/client/theme-default/styles/utils.css'
 import 'vitepress/dist/client/theme-default/styles/components/custom-block.css'
 import 'vitepress/dist/client/theme-default/styles/components/vp-code-group.css'
